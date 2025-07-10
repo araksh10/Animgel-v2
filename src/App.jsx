52e7c342fb4074c94bd756b1aec3b775b621ce12
Abir Rakshit <abir.raksh@gmail.com>
2025-07-10 22:04:00 +0600

    Connection: Database api connected

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-	uri: "http://localhost:4000/graphql",
+	uri: "https://animgel-api-v2.onrender.com/graphql",
 	cache: new InMemoryCache(),
 });
 
